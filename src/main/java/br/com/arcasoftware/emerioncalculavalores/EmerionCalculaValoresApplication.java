3472b3915b2c74da6ce733a336f591e147731b1b
Arnaldo C. Bezerra <arnaldotecadm@hotmail.com>
2021-03-28 22:42:38 -0300

    first commit

---
+++ src/main/java/br/com/arcasoftware/emerioncalculavalores/EmerionCalculaValoresApplication.java
@@ -0,0 +1,13 @@
+package br.com.arcasoftware.emerioncalculavalores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmerionCalculaValoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmerionCalculaValoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/arcasoftware/emerioncalculavalores/EmerionCalculaValoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arcasoftware.emerioncalculavalores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmerionCalculaValoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
